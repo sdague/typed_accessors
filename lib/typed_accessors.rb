4467ce9d54b5827412f8f5c01e67c4a78ec995f1
Sean Dague <sean@dague.net>
2009-03-27 07:32:08 -0400

    added documentation, should add some tests and push out a 0.2

--- lib/typed_accessors.rb
+++ lib/typed_accessors.rb
@@ -1,19 +1,46 @@
+# Typed accessors works as a Mixin on Class.  It creates a set of
+# functions that are used in the class definition which eval and
+# create new accessor methods at class eval time (so little
+# performance penalty).  For now, this is limitted to built in types,
+# float, int, boolean, and date, though this approach could clearly be
+# expanded.
+#
+# Example of a class definition
+#
+#   class MyClass
+#     float_accessor :distance
+#     int_accessor :count
+#     bool_yn_accessor :onfire?
+#     date_accessor :day
+#   end
+
 class Class
+    
+    # Creates a boolean accessor.  It will convert and incoming string
+    # to a true / false value.  If the string is "y" or "Y" it will be
+    # true, otherwise false.
     def bool_yn_accessor( *symbols )
         attr_reader( *symbols )
         bool_yn_writer( *symbols )
     end
 
+    
+    # Creates a float accessor, using built in .to_f functions on
+    # objects.  Any object that has a .to_f is supported.
     def float_accessor( *symbols )
         float_reader( *symbols )
         float_writer( *symbols )
     end
     
+    # Creates an int accessor, using built in .to_i functions on
+    # objects.  Any object that has a .to_i is supported.
     def int_accessor( *symbols )
         int_reader( *symbols )
         int_writer( *symbols )
     end
     
+    # Creates a data accessor using the Date parse function on
+    # strings.  Not defined for other input types.
     def date_accessor( *symbols )
         attr_reader( *symbols )
         date_writer( *symbols )
